33794dd4ff5c74aaf919b262a49a60ccae4bc2f1
Hristo <hmaleev@gmail.com>
2014-07-17 11:41:57 +0300

    updated Convertor
    code refactoring

--- Convertor/www/js/tempertatureConversion.js
+++ Convertor/www/js/tempertatureConversion.js
@@ -1,8 +1,56 @@
-
-
 var btn = $("#temperatureConversionBtn");
 
-btn.click( function () {
+
+btn.click(function() {
+	var conversion = new ConversionModule.TemperatureConverion();
+	alert(conversion.convert());
+})
+
+var ConversionModule = (function() {
+
+	function convertCurrentUnitToCelsius(unit, currentValue) {
+		//currentUnitValue = parseFloat(currentUnitValue);
+		switch (unit) {
+			case "Celsius":
+				return currentValue;
+			case "Kelvin":
+				return currentValue - 273.15;
+			case "Fahrenheit":
+				return (currentValue - 32.0) / 1.8;
+		}
+	}
+
+	function convertCelsiusToNewTemperatureUnit(newUnit, temporaryValue) {
+		switch (newUnit) {
+			case "Celsius":
+				return temporaryValue;
+			case "Kelvin":
+				return temporaryValue + 273.15;
+			case "Fahrenheit":
+				return (temporaryValue * 1.8) + 32.0;
+		}
+	}
+	return {
+		TemperatureConverion: function() {
+
+			this.convert = function() {
+
+				var currentUnit = $("#originalTemperatureUnit").val();
+				var currentValue = parseFloat($("#temperatureValue").val());
+				var newUnit = $("#convertedTemperatureUnit").val();
+				var temporaryValue = convertCurrentUnitToCelsius(currentUnit, currentValue);
+				var finalValue = convertCelsiusToNewTemperatureUnit(newUnit, temporaryValue).toFixed(2);
+
+				return finalValue;
+
+			};
+		}
+	};
+}());
+
+
+//-------------ORIGINAL CODE------------------
+/*
 
 	var currentUnit = $("#originalTemperatureUnit").val();
 	var currentUnitValue = parseFloat($("#temperatureValue").val());
@@ -21,9 +69,6 @@ btn.click( function () {
 			case "Kelvin": return temporaryValue+273.15;
 			case "Fahrenheit": return (temporaryValue*1.8 )+32.0; 
 		}
-	
 	}
 	var temporaryValue = parseFloat(convertCurrentUnitToCelsius());
-
-	alert(convertCelsiusToNewTemperatureUnit());
-})
+	*/
