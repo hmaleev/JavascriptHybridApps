ad147cc5938155abc769ec6745135df8b26caeb2
Hristo <hmaleev@gmail.com>
2014-10-07 00:14:20 +0300

    updated Convertor

--- Convertor/platforms/android/assets/www/cordova_plugins.js
+++ Convertor/platforms/android/assets/www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/com.rjfun.cordova.plugin.flurry/www/Flurry.js",
+        "id": "com.rjfun.cordova.plugin.flurry.Flurry",
+        "clobbers": [
+            "window.plugins.Flurry"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.network-information/www/network.js",
         "id": "org.apache.cordova.network-information.network",
         "clobbers": [
@@ -28,24 +35,17 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
-    },
-    {
-        "file": "plugins/com.rjfun.cordova.plugin.flurry/www/Flurry.js",
-        "id": "com.rjfun.cordova.plugin.flurry.Flurry",
-        "clobbers": [
-            "window.plugins.Flurry"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "com.flurry.sdk-android": "4.0.0",
+    "com.google.admobsdk-googleplay": "5.0.89",
+    "com.rjfun.cordova.plugin.flurry": "1.0.0",
     "org.apache.cordova.network-information": "0.2.10",
     "org.apache.cordova.splashscreen": "0.3.1",
-    "org.apache.cordova.vibration": "0.3.10-dev",
-    "com.rjfun.cordova.plugin.flurry": "1.0.0",
-    "com.google.admobsdk-googleplay": "5.0.89",
-    "com.flurry.sdk-android": "4.0.0"
+    "org.apache.cordova.vibration": "0.3.10-dev"
 }
 // BOTTOM OF METADATA
 });
